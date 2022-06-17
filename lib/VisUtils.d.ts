48cdea345d3e67e942d2905e326fa6fa6bee4aa4
Daniel Kerrigan <d.kerrigan111@gmail.com>
2022-06-17 15:42:43 -0400

    formatting, remove files unnecessary files

--- lib/VisUtils.d.ts
+++
@@ -1,2 +0,0 @@
-export { scaleCanvas };
-declare function scaleCanvas(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, width: number, height: number): void;
--- lib/plugin.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Application, IPlugin } from '@phosphor/application';
-import type { Widget } from '@phosphor/widgets';
-/**
- * The example plugin.
- */
-declare const examplePlugin: IPlugin<Application<Widget>, void>;
-export default examplePlugin;
